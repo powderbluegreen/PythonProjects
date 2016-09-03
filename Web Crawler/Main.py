3a8a06bea2b12def21d7b08ce226995f85b9ac28
Andrew Smith <Andrew Smith>
2016-09-03 01:29:01 -0500

    Finished Date logic

--- Web Crawler/Main.py
+++ Web Crawler/Main.py
@@ -5,22 +5,33 @@ import os
 from bs4 import BeautifulSoup
 import urllib
 import re
+import time
 
-
-
+base_url = 'https://www.stpauls.com/weekly-newsletter-for-'
 def getDays():
-    date = datetime.date.today()
+    '''date = datetime.date.today()
     date_re = re.findall('-(\d\d)-', str(date))
     date_re = str(date_re).rstrip('\'\]')
     date_re = date_re.lstrip('\[\'')
     month = int(date_re)
     monthAbbr = calendar.month_abbr[month]
-    print monthAbbr
+    print monthAbbr'''
+    cal = calendar.Calendar(6)
+    weekList = cal.monthdatescalendar(int(time.strftime('%Y')), int(time.strftime('%m')))
+    return weekList
 
 
-''' def getUrl():
-    base_url = 'https://www.stpauls.com/weekly-newsletter-for-'
-    html = requests.get(base_url).content
+def getUrl(month, day, month2, day2):
+    if len(day) == 1:
+        day = day.zfill(2)
+    if len(day2) == 1:
+        day2 = day2.zfill(2)
+    if month == month2:
+        final_url = base_url + str(calendar.month_abbr[int(month)]).lower() + '-' + day + '-' + day2 + '\\'
+    else:
+        final_url = base_url + str(calendar.month_abbr[int(month)]).lower() + '-' + day + '-' + \
+                               str(calendar.month_abbr[int(month2)]).lower() + '-' + day2 + '/'
+    html = requests.get(final_url).content
     return html
 
 
@@ -36,7 +47,6 @@ def parseHtml(html):
 def getPDF(link):
     os.chdir('C:' + os.getenv('HOMEPATH') + '\\Desktop')
     urllib.urlretrieve(link, 'Newsletter.pdf')
-'''
 
 '''osenv = os.getenv('HOMEPATH')
     os.chdir('C:' + osenv + '\\Desktop')
@@ -44,8 +54,13 @@ def getPDF(link):
     file2.write(str(soup))'''
 
 
+weekList = getDays()
+try:
+    getPDF(parseHtml(getUrl(str(weekList[0][0].month), str(weekList[0][1].day),
+    str(weekList[0][6].month), str(weekList[0][7].day))))
+except AttributeError as e:
+    print "Unfortunately, this program does not keep track of all holidays that the school follows, so the newsletter cannot be downloaded"
+    print e
 
 
 
-# getPDF(parseHtml(getUrl()))
-getDays()
