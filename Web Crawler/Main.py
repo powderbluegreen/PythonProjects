37212a6e6c54843fb07ac9075b7e2793dba09e4c
Andrew Smith <powederbluegeeen@gmail.com>
2016-09-02 23:35:51 -0500

    Added month abbr. logic

--- Web Crawler/Main.py
+++ Web Crawler/Main.py
@@ -1,12 +1,25 @@
 import requests
+import calendar
 import datetime
 import os
 from bs4 import BeautifulSoup
+import urllib
 import re
 
 
-def getUrl():
-    base_url = 'https://www.stpauls.com/weekly-newsletter-for-aug-22-28'
+
+def getDays():
+    date = datetime.date.today()
+    date_re = re.findall('-(\d\d)-', str(date))
+    date_re = str(date_re).rstrip('\'\]')
+    date_re = date_re.lstrip('\[\'')
+    month = int(date_re)
+    monthAbbr = calendar.month_abbr[month]
+    print monthAbbr
+
+
+''' def getUrl():
+    base_url = 'https://www.stpauls.com/weekly-newsletter-for-'
     html = requests.get(base_url).content
     return html
 
@@ -19,12 +32,20 @@ def parseHtml(html):
     link = divPost_pa['href']
     return link
 
-def getPDF():
 
-    '''osenv = os.getenv('HOMEPATH')
+def getPDF(link):
+    os.chdir('C:' + os.getenv('HOMEPATH') + '\\Desktop')
+    urllib.urlretrieve(link, 'Newsletter.pdf')
+'''
+
+'''osenv = os.getenv('HOMEPATH')
     os.chdir('C:' + osenv + '\\Desktop')
     file2 = open('test2.txt', 'w')
     file2.write(str(soup))'''
 
 
-parseHtml(getUrl())
+
+
+
+# getPDF(parseHtml(getUrl()))
+getDays()
